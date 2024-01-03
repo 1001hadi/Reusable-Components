ea1ac8f67edf5ba33b91bdd9e7ac04a43e3bf625
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-03 15:24:53 -0700

    start of redux

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,92 @@
-import useCounter from "../hooks/use-counter";
+import { useReducer } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
+
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const VALUE_TO_ADD = "change_value-to-add";
+
+const reducer = (state, action) => {
+  if (action.type === INCREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  if (action.type === DECREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  }
+
+  if (action.type === VALUE_TO_ADD) {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  }
+
+  return state;
+};
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  };
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
+  };
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value) || 0;
+
+    dispatch({
+      type: VALUE_TO_ADD,
+      payload: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
+  };
+
   return (
-    <div>
-      <h1>Count is: {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is: {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add A lot</label>
+        <input
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button success rounded>
+          Add it!
+        </Button>
+      </form>
+    </Panel>
   );
 }
 
